a3b86e8f70aa55e63a7920607d420f28826f904f
bbrecht02 <bennyson.bgcorreia@gmail.com>
2022-01-08 17:27:02 -0300

    update ex

--- functions.js
+++ functions.js
@@ -20,8 +20,37 @@ sayMyName(
 
 // construtor 
 
-let Person = (nome) => {
-    this.nome = nome
+function Person(nome) {
+    this.nome = nome;
 }
 
-let Benny = new Person("benny")
+let benny = new Person("benny");
+console.log(benny);
+
+
+// advanced topics 
+
+//arrow funcs 
+
+let sum = (a,b) => a + b;
+
+console.log(sum(1,5));
+
+// hoisting
+
+// function call before create
+log("teste");
+
+function log(value) {
+    console.log(value);
+};
+
+// object
+
+let obj = {
+    sum: function sum(a, b) {
+        return a + b;
+    }
+};
+
+console.log(obj.sum(1,5));
